61ae4b20f75b198c83188daea315d9a55c985de6
Ronny0823 <reyesronny42@gimail.com>
2019-06-05 12:33:29 -0400

    javascript

---
+++ javascript_101/0_script.js
@@ -0,0 +1,43 @@
+//alert("Hola a todos!");
+/* Pedir al usuario que ingrese su edad*/ 
+//soy un comentario de 1 solo linea
+var nombre=prompt("Digite su nombre");
+var edad=prompt("Digite su Edad");
+var ciudad=prompt("Digite su Ciudad");
+var credito=prompt("Digite su Tarjeta de Credito");
+
+// Imprimir los valores por la consola
+console.log(nombre)
+console.log(edad)
+console.log(ciudad)
+console.log(credito)
+
+
+console.log(document.documentURI);
+
+console.log(document.body);
+
+console.log(document.head);
+
+console.log(document.height);
+
+console.log(document.URL);
+
+console.log(document.title);
+
+
+
+
+// Esta variable no esta definida
+var pais;
+console.log(pais);
+
+// La variable esta definida
+var pais = "Rep.Dom";
+console.log(pais);
+
+// Cambiar el titulo
+document.title = "Nuevo titulo";
+
+
+
---
+++ javascript_101/1_ejercicio.js
@@ -0,0 +1,13 @@
+//soy un comentario de 1 sola linea
+var nombre =prompt("Digite su nombre");
+var edad =prompt("digite su edad")
+document.write(`<h1>${nombre}</h1>`)
+document.write(`<p>${edad}</p>`)
+
+// 2. Hacer que el usuario digite
+var ciudad =prompt("Digite su ciudad");
+var barrio =prompt("Digite su barrio");
+var direccion =prompt("Digite su direcion");
+document.write(`<div>${ciudad}</div>`)
+document.write(`<div>${barrio}</div>`)
+document.write(`<div>${direcion}</div>`)
