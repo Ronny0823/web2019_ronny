f57585a29cb261a164f9f0b5bbfb912a53aec99f
Ronny0823 <reyesronny42@gmail.com>
2019-06-10 06:23:00 -0400

    tarea y logo

---
+++ tarea/0_tarea.js
@@ -0,0 +1,17 @@
+var nombre=prompt('Digite su nombre');
+var edad=prompt('Digite su edad');
+var telefono=prompt('Digite su telefono');
+
+console.log=(nombre);
+console.log=(edad);
+console.log=(telefono);
+
+document.write(`<div>${nombre}</div>`);
+document.write(`<div>${edad}</div>`);
+document.write(`<div>${telefono}</div>`);
+
+alert('Hola Mundo')
+
+
+
+
